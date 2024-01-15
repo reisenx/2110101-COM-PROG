d74f9f561e343fde2948a5d70348eb7c848419c6
Worralop Srichainont <bloomrock555@gmail.com>
2024-01-15 13:20:14 +0700

    Grader 04 commit
    - Added 3 Python file in 04 Repetition

---
+++ 04 Repetition/04_Loop_03.py
@@ -0,0 +1,18 @@
+#Input 2 string
+#Set the initial score to 0
+key = str(input())
+answer = str(input())
+score = 0
+
+#Check if the answer is incomplete answer
+#Incomplete answer is the length of the string is not equal
+if(len(key)!=len(answer)):
+    print("Incomplete answer")
+#Complete answer
+else:
+    #If the answer string and key string in index i is the same, add score by 1
+    for i in range(0,len(key)):
+        if(answer[i]==key[i]):
+            score = score+1
+    #Output the score
+    print(score)
---
+++ 04 Repetition/04_Loop_05.py
@@ -0,0 +1,23 @@
+#Input 2 string
+word = str(input())
+sentence = str(input())
+
+#Replace symbol from the string to " "
+symbol = ["\"", "(", ")", ",", ".", "\""]
+for i in range (0,len(sentence)):
+    if(sentence[i] in symbol):
+        sentence = sentence[:i] + " " + sentence[i+1:]
+
+#Split a sentences into a list
+list_word = sentence.split()
+
+#Set initial word count = 0
+count = 0
+
+#Check how many word in a list is the same to input word 
+for i in range(0,len(list_word)):
+    if(word == list_word[i]):
+        count = count+1
+
+#Output the word count
+print(count)
