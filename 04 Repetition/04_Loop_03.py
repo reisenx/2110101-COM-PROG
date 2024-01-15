9d1928444d5a77708da05f53041216153eb11337
Worralop Srichainont <bloomrock555@gmail.com>
2024-01-15 13:56:47 +0700

    04 Grader Commit
    - Add 3 Python file to 04 Repetition
    - Update 1 Python file to 04 Repetition

--- 04 Repetition/04_Loop_03.py
+++ 04 Repetition/04_Loop_03.py
@@ -6,13 +6,13 @@ score = 0
 
 #Check if the answer is incomplete answer
 #Incomplete answer is the length of the string is not equal
-if(len(key)!=len(answer)):
+if(len(key) != len(answer)):
     print("Incomplete answer")
 #Complete answer
 else:
     #If the answer string and key string in index i is the same, add score by 1
     for i in range(0,len(key)):
-        if(answer[i]==key[i]):
+        if(answer[i] == key[i]):
             score = score+1
     #Output the score
     print(score)
---
+++ 04 Repetition/04_Loop_F06.py
@@ -0,0 +1,19 @@
+#Create a function print_triangle(n)
+def print_triangle(n):
+    #Output the triangle
+    #A triangle height is n
+    #Output the first n-1 lines
+    for i in range (1,n):
+        #Part 1: Print "." outside the triangle
+        #Part 2: Print "*" (egde of the triangle)
+        #Part 3: Print "." inside the triangle
+        #Part 4: Print "*" (another edge of triangle)
+        if(i==1):
+            print((n-i)*"." + "*")
+        elif(i!=1):
+            print((n-i)*"." + "*" + (2*(i-1)-1)*"." + "*")
+    #Output the last line
+    print((2*n-1)*"*")
+
+#Execute input string
+exec(input())
