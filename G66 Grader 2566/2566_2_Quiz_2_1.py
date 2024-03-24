3f6b23f8bbab4fe2c7faaea4d449511221b037b8
Worralop Srichainont <bloomrock555@gmail.com>
2024-03-25 02:52:39 +0700

    Grader 02 2566 Solution
    - Added 4 Python files in G66 Grader 2566

--- G66 Grader 2566/2566_2_Quiz_2_1.py
+++ G66 Grader 2566/2566_2_Quiz_2_1.py
@@ -1,3 +1,5 @@
+# This function can replace all vowels ('a','e','i','o','u') to '*'
+# Example: hide_vowel("FuCk sHiT") returns "F*Ck sH*T"
 def hide_vowel(w):
     h = ""
     for c in w:
@@ -6,24 +8,49 @@ def hide_vowel(w):
         h += c
     return h
 
+# This function can censor 'oword' that appears in 't'
+# Example: less_offensive("He's so full of crap.","Crap") returns "He's so full of cr*p."
+
+# Algorithm
+# Create a new string 'censor'
+# Check if t[start:end] is 'oword' until the end of the string 't' 
+# - If yes, add censored t[start:end] to 'censor' and increase start and end by len(oword)
+# - If no, add t[start] to 'censor' and increase start and end by 1
+
+# Example: less_offensive("He's so full of crap.","Crap")
+# Loop 1: t[0:4] = "[He's] so full of crap." --> censor = "H"
+# Loop 2: t[1:5] = "H[e's ]so full of crap." --> censor = "He"       
+# Loop 3: t[2:6] = "He['s s]o full of crap." --> censor = "He'"      
+# ...
+# Loop 16: t[15:19] = "He's so full of[ cra]p." --> censor = "He's so full of "
+# Loop 17: t[16:20] = "He's so full of [crap]." --> censor = "He's so full of cr*p" (Added 'cr*p' to censor)
+# Loop 18: t[20:24] = "He's so full of crap[.]" --> censor = "He's so full of cr*p."
+# Loop 19: t[21:25] --> break (start = 21 and len(t) = 21)
 def less_offensive(t,oword):
-    index = 0
+    start = 0
     end = len(oword)
     censor = ""
-    while(index<len(t)):
-        if(t[index:end].lower() == oword.lower()):
-            censor += hide_vowel(t[index:end])
-            index += len(oword)
+    while(start < len(t)):
+        if(t[start:end].lower() == oword.lower()):
+            censor += hide_vowel(t[start:end])
+            start += len(oword)
             end += len(oword)
         else:
-            censor += t[index]
-            index += 1
-            end +=1
+            censor += t[start]
+            start += 1
+            end += 1
     return censor
 
+# Input offensive words and split them to a list
 Owords = input().split()
+# Input a text that need to be censored
 text = input()
+# Censor each word in a 'Owords' list
+# Example: Owords = ['crap', 'DAMN'] and censor = "!!!CrapDAMNcRAPdamn_crab_darn!!!"
+# 1.) less_offensive(censor, 'crap') returns censor = "!!!Cr*pDAMNcR*Pdamn_crab_darn!!!"
+# 2.) less_offensive(censor, 'DAMN') returns censor = "!!!Cr*pD*MNcR*Pd*mn_crab_darn!!!"
 censor = text
 for word in Owords:
     censor = less_offensive(censor,word)
+# Output
 print(censor)
--- G66 Grader 2566/2566_2_Quiz_2_3.py
+++ G66 Grader 2566/2566_2_Quiz_2_3.py
@@ -1,15 +1,72 @@
+# Example testcase filename 1: text1.txt
+# Example testcase filename 2: text2.txt
+
+# Open a file
 filename = input().strip()
 file = open(filename,"r")
 
+# Read every line in a file, split them with '_' then convert them all to a list of words
 words = []
 for line in file:
+    # Strip a line to get rid of "\n" at the end of each line
     line = line.strip()
+    # Considered only non-empty line
     if(line != ""):
         temp = line.split("_")
         for item in temp:
             words.append(item)
 
+# Output
+# In the first line, output 50 "-"
 print("-"*50)
+
+# Algorithm
+# 1.) Setup inital value start = 0 and end = 0
+# 2.) Find the length of line = "_".join(words[start:end])
+# - If the length is less than 50, Increase 'end' by 1
+# - If the length is more than 50, Output "_".join(words[start:end-1])
+#   and setup start = end-1 and end = end-1
+# 3.) If it reached the end of the 'words' list and the length less than 50, output it then break
+
+# Example:
+# words = ['Mary','had','a','little','lamb','little','lamb','little','lamb','Mary','had','a','little',
+#          'lamb','its','fleece','was','white','as','snow']
+
+# Process
+# Loop 1: start = 0 | end = 0 | Length = 0
+# > line = ""
+# Loop 2: start = 0 | end = 1 | Length = 4
+# > line = "Mary"
+# Loop 3: start = 0 | end = 2 | Length = 8
+# > line = "Mary_had"
+# ...
+# Loop 10: start = 0 | end = 9 | Length = 46
+# > line = "Mary_had_a_little_lamb_little_lamb_little_lamb"    
+# Loop 11: start = 0 | end = 10 | Length = 51
+# > line = "Mary_had_a_little_lamb_little_lamb_little_lamb_Mary"
+# > Output "Mary_had_a_little_lamb_little_lamb_little_lamb"
+# > Setup start = 9, end = 9
+
+# Loop 12: start = 9 | end = 9 | Length = 0
+# > line = ""
+# Loop 13: start = 9 | end = 10 | Length = 4
+# > line = "Mary"
+# Loop 14: start = 9 | end = 11 | Length = 8
+# > line = "Mary_had"
+# ...
+# Loop 22: start = 9 | end = 19 | Length = 46
+# > line = "Mary_had_a_little_lamb_its_fleece_was_white_as"
+# Loop 23: start = 9 | end = 20 | Length = 51
+# > line = "Mary_had_a_little_lamb_its_fleece_was_white_as_snow"
+# > Output "Mary_had_a_little_lamb_its_fleece_was_white_as"
+# > Setup start = 19, end = 19
+
+# Loop 24: start = 19 | end = 19 | Length = 0
+# > line = ""
+# Loop 25: start = 19 | end = 20 | Length = 4
+# > line = "snow"
+# Loop 26: start = 19 | end = 21 (end > len(words))
+# > Output "snow", then break a loop
 start = 0
 end = 0
 while(start < len(words)):
@@ -24,4 +81,5 @@ while(start < len(words)):
         start = end - 1
         end = end - 1
 
+# Close a file
 file.close()
