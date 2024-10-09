b8a612ed86ca949aadc9af215e9097d1054708dc
reisenx <bloomrock555@gmail.com>
2024-10-09 15:12:18 +0700

    Update 09_​MoreDC_​31.py

--- 09 Nested Structure/09_​MoreDC_​31/09_​MoreDC_​31.py
+++ 09 Nested Structure/09_​MoreDC_​31/09_​MoreDC_​31.py
@@ -20,44 +20,27 @@ def is_coprime(a,b,c):
 # If we loop a,b,c from 1 to max_len, it would cause a Runtime Exceed (T)
 # So we need to find another algorithm
 
-# Euclid's formula for generating Pythagorean triples
-# Given integer m,n that 0 < n < m
-# From (m^2)^2 + 2(m^2)(n^2) + (n^2)^2 = ((m^2)^2 - 2(m^2)(n^2) + (n^2)^2) + 4(m^2)(n^2)
-#      (m^2 + n^2)^2 = (m^2 - n^2)^2 + (2mn)^2
-# From the equation above, we can substitute a,b,c as
-# 1.) c = m^2 + n^2
-# 2.) b = 2mn
-# 3.) a = m^2 - n^2
+# From a^2 + b^2 = c^2 we can know that c = (a^2 + b^2)^0.5
+# To check if c is an integer, we can use c == int(c)
 def primitive_Pythagorean_triples(max_len):
-    # Create a list to contain Pythagorean triples
     temp = []
     triples = []
-    # Set initial value m = 2
-    m = 2
-    a,b,c = 0,0,0
-    # Loop until a,b,c is greater than max_len
-    while(a <= max_len or b <= max_len or c <= max_len):
-        # Loop n = 1,2,3,...,m-1 (0 < n < m)
-        for n in range(1,m):
-            c = (m**2) + (n**2)
-            b = 2*m*n
-            a = (m**2) - (n**2)
-            
-            # Check if a,b,c is greater than max_len
-            if(a > max_len and b > max_len and c > max_len):
-                break
-            # Make sure than 0 <= a <= b <= c
-            if(not (a <= b <= c)):
-                a,b,c = sorted([a,b,c])
-            # Check if the (a,b,c) is Primitive Pythagorean triple and c <= max_len
-            if(is_coprime(a,b,c) and c <= max_len):
+    for a in range(1, max_len+1):
+        for b in range(1, max_len+1):
+            c = (a**2 + b**2)**(0.5)
+            # Conditions
+            # 1.) c <= max_len
+            # 2.) a^2 + b^2 = c^2 (c must be an integer)
+            # 3.) a,b,c must be coprime
+            if(c <= max_len and c == int(c) and is_coprime(a,b,c)):
+                # Prevent cases when b < a
+                a,b,c = sorted([a,b,int(c)])
                 # Append in [c,a,b] format for sorting purpose
-                temp.append([c,a,b])
-        # Increase m by 1 until a,b,c > max_len
-        m += 1
-    # Sorting by 'c'. If 'c' is equal, then sorting by 'a'
+                if([c,a,b] not in temp):
+                    temp.append([c,a,b])
     temp.sort()
-    # Rearrange from [c,a,b] format to [a,b,c] format
+    
+    # Convert to [a,b,c] format
     for c,a,b in temp:
         triples.append([a,b,c])
     return triples
