979da087cb04303fc9e22ad1aea03b7dc7b14e58
reisenx <bloomrock555@gmail.com>
2025-06-10 22:07:34 +0700

    fix: formatting 02_strlist_f06 solution

--- 02-Basic-String-and-List/02_StrList_F06/02_StrList_F06.py
+++ 02-Basic-String-and-List/02_StrList_F06/02_StrList_F06.py
@@ -1,13 +1,19 @@
-# Create Function add_vector(u,v)
-# This function can calculate a sum of 2 input vectors
-def add_vector(u,v):
-    # Convert a string list into a float list
-    ux,uy,uz = float(u[0]), float(u[1]), float(u[2])
-    vx,vy,vz = float(v[0]), float(v[1]), float(v[2])
+# --------------------------------------------------
+# File Name : 02_StrList_F06.py
+# Problem   : Add Vector (Function)
+# Author    : Worralop Srichainont
+# Date      : 2025-06-10
+# --------------------------------------------------
 
-    # Calculate and return the sum vector
-    vec_sum=[ux+vx, uy+vy, uz+vz]
-    return vec_sum
 
-# Execute the input string
+# Create a function to add two vectors
+def add_vector(u: list[float], v: list[float]) -> list[float]:
+    total = [0.0, 0.0, 0.0]
+    total[0] = float(u[0]) + float(v[0])
+    total[1] = float(u[1]) + float(v[1])
+    total[2] = float(u[2]) + float(v[2])
+    return total
+
+
+# Execute the input string as code
 exec(input())
