36f4e552ad0eb49000a3231c4a7489f9557e8147
reisenx <bloomrock555@gmail.com>
2025-06-11 15:09:31 +0700

    fix: formatting 03_if_01 solution

--- 03-Selection/03_If_01/03_If_01.py
+++ 03-Selection/03_If_01/03_If_01.py
@@ -1,12 +1,46 @@
-# Create a list the contains Chula faculty number
-faculty_num = ["01","02","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","51","53","55","58"]
+# --------------------------------------------------
+# File Name : 03_If_01.py
+# Problem   : Faculty Code
+# Author    : Worralop Srichainont
+# Date      : 2025-06-10
+# --------------------------------------------------
 
-# Input the code as a string
-code = str(input())
+# List of valid faculty codes
+FACULTIES = [
+    "01",
+    "02",
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+    "25",
+    "26",
+    "27",
+    "28",
+    "29",
+    "30",
+    "31",
+    "32",
+    "33",
+    "34",
+    "35",
+    "36",
+    "37",
+    "38",
+    "39",
+    "40",
+    "51",
+    "53",
+    "55",
+    "58",
+]
 
-# Check if the code is in the list
-# If true, output "OK" but if not, output "Error" 
-if(code in faculty_num):
+# Input faculty code
+code = input().strip()
+
+# Check if the code is valid
+if code in FACULTIES:
     print("OK")
 else:
     print("Error")
