404d3c591d4842e0d346644ca3b7a32686a42134
reisenx <bloomrock555@gmail.com>
2025-06-11 15:10:19 +0700

    fix: formatting 03_if_03 solution

--- 03-Selection/03_If_03/03_If_03.py
+++ 03-Selection/03_If_03/03_If_03.py
@@ -1,30 +1,39 @@
-# Input score and convert to float
-a,b,c,d = [float(e) for e in input().split()]
+# --------------------------------------------------
+# File Name : 03_If_03.py
+# Problem   : Gymnastic Score
+# Author    : Worralop Srichainont
+# Date      : 2025-06-10
+# --------------------------------------------------
 
-# Find sum of all score
-sum = a + b + c + d
+# Input the scores of 4 judges
+score = input().strip().split()
+score[0] = float(score[0])
+score[1] = float(score[1])
+score[2] = float(score[2])
+score[3] = float(score[3])
 
-# Find minimum score
-minimum = a
-if(b < minimum):
-    minimum = b
-if(c < minimum):
-    minimum = c
-if(d < minimum):
-    minimum = d
+# Find the maximum and minimum scores
+max_score = score[0]
+min_score = score[0]
 
-# Find maximum score
-maximum = a
-if(b > maximum):
-    maximum = b
-if(c > maximum):
-    maximum = c
-if(d > maximum):
-    maximum = d
+if score[1] > max_score:
+    max_score = score[1]
+if score[1] < min_score:
+    min_score = score[1]
 
-# Subtract minimum score and maximum score from 'sum'
-sum -= minimum + maximum
+if score[2] > max_score:
+    max_score = score[2]
+if score[2] < min_score:
+    min_score = score[2]
 
-# Find average score
-average = round(sum/2, 2)
-print(average)
+if score[3] > max_score:
+    max_score = score[3]
+if score[3] < min_score:
+    min_score = score[3]
+
+# Calculate the average score
+score_total = score[0] + score[1] + score[2] + score[3]
+average_score = (score_total - max_score - min_score) / 2
+
+# Output the average score
+print(round(average_score, 2))
