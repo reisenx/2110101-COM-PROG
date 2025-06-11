eef7d665e9ff9ea36159e006ca7f573479d40596
reisenx <bloomrock555@gmail.com>
2025-06-11 15:09:11 +0700

    fix: formatting 03_if_003 solution

--- 03-Selection/03_If_003/03_If_003.py
+++ 03-Selection/03_If_003/03_If_003.py
@@ -1,43 +1,29 @@
-# Flowchart
+# --------------------------------------------------
+# File Name : 03_If_003.py
+# Problem   : Flowchart 01
+# Author    : Worralop Srichainont
+# Date      : 2025-06-10
+# --------------------------------------------------
 
-# Input a,b,c,d value
-a,b,c,d = [int(e) for e in input().split()]
+a, b, c, d = [int(e) for e in input().split()]
 
-# Condition: a>b
-# If true, swap a,b value then check the following condition
-# If false, check the following condition
-if(a > b):
-    a,b = b,a
-    # Condition: d>=a
-    # If true, check the following condition
-    # If false, change the value of c to c+a
-    if(d >= a):
-        # Condition: c>d
-        # If true, change the value of c to c-a
-        # If false, do nothing
-        if(c > d):
-            c = c-a
-        else:
-            pass
+if a > b:
+    a, b = b, a
+
+    if not d >= a:
+        c = c + a
     else:
-        c = c+a
-    # Change the value b to a+c+d
-    b = a+c+d
+        if c > d:
+            c = c - a
+
+    b = a + c + d
 else:
-    # Condition: c>a>=b
-    # If true, change the value of d to d+a
-    # If false, do nothing
-    if(c > a and a >= b):
-        d = d+a
-    else:
-        pass
-    # Condition: d>c
-    # If true, change the value of b to b+2
-    # If false, change the value of b to 2*b
-    if(d > c):
-        b = b+2
-    else:
-        b = 2*b
+    if c > a >= b:
+        d = d + a
 
-# Output the value of a,b,c and d
-print(a,b,c,d)
+    if d > c:
+        b = b + 2
+    else:
+        b = 2 * b
+
+print(a, b, c, d)
