c41f28efbbd0fcd7f299347f90af02a08fac1801
reisenx <bloomrock555@gmail.com>
2025-06-11 15:08:52 +0700

    fix: formatting 03_if_002 solution

--- 03-Selection/03_If_002/03_If_002.py
+++ 03-Selection/03_If_002/03_If_002.py
@@ -1,59 +1,36 @@
-# Flowchart
+# --------------------------------------------------
+# File Name : 03_If_002.py
+# Problem   : Next 15 Days (Flowchart)
+# Author    : Worralop Srichainont
+# Date      : 2025-06-10
+# --------------------------------------------------
 
-# Input a string in DD MM YYYY format 
-# Then split into 3 part and contain them in d,m,y variable
-d,m,y = [int(e) for e in input().split()]
+d, m, y = [int(e) for e in input().split()]
 
-# Create a list of 30-day month
-m30list = [4,6,9,11]
+y = y - 543
 
-# Change the value of y to y-543
-y = y-543
-
-# Set the value of n=31
 n = 31
 
-# Condition: input month (m) is the 30-day month
-# If true, change the value of n to 30
-# If false, check the following condition
-if(m in m30list):
+if m in [4, 6, 9, 11]:
     n = 30
 else:
-    # Condition: m = 2
-    # If true, change the value of n to 28 then check the following condition
-    # If false, do nothing
-    if(m == 2):
+    if m == 2:
         n = 28
-        # Condition: y mod 400 = 0
-        # If true, change the value of n to 29
-        # If false, do nothing
-        if(y%400 == 0):
+        if y % 400 == 0:
             n = 29
-        # Condition: y mod 4 = 0 and y mod 100 != 0
-        # If true, change the value of n to 29
-        # If false, do nothing
-        if(y%4 == 0 and y%100 != 0):
+        if y % 4 == 0 and y % 100 != 0:
             n = 29
 
-# Change the value of d to d+15
-d += 15
+d = d + 15
 
-# Condition: d > n
-# If true, Change the value of d to d-n and the value of m to m+1
-# If false, do nothing
-if(d > n):
-    d = d-n
-    m += 1
+if d > n:
+    d = d - n
+    m = m + 1
 
-# Condition: m > 12
-# If true, Change the value of m to m-12 and the value of y to y+1
-# If false, do nothing
-if(m > 12):
-    m -= 12
-    y += 1
+if m > 12:
+    m = m - 12
+    y = y + 1
 
-# Change the value of y to y+543
-y += 543
+y = y + 543
 
-# Output the date in specific form
 print(str(d) + "/" + str(m) + "/" + str(y))
