0b7febae69ac9285bc2e110f44c2af9bf0f5cb66
reisenx <bloomrock555@gmail.com>
2025-06-11 19:11:21 +0700

    fix: formatting 04_loop_12 solution

--- 04-Repetition/04_Loop_12/04_Loop_12.py
+++ 04-Repetition/04_Loop_12/04_Loop_12.py
@@ -1,33 +1,30 @@
-# Input a number of data lines
-N = int(input())
+# --------------------------------------------------
+# File Name : 04_Loop_12.py
+# Problem   : Zig Zag 1
+# Author    : Worralop Srichainont
+# Date      : 2025-06-11
+# --------------------------------------------------
 
-# Create an empty list
-X = []
-Y = []
-list_01 = []
-list_02 = []
+# Input number of rows
+n = int(input())
 
-# Input a data, split them and put it into a list
-for i in range(0,N):
-    data = [int(e) for e in input().split()]
-    X.append(data[0])
-    Y.append(data[1])
+# Initialize lists
+A = []
+B = []
 
-# List 01: X0,Y1,X2,Y3,X4,Y5,... (Index starts at 0)
-# List 02: Y0,X1,Y2,X3,Y4,X5,... (Index starts at 0)
-for i in range(0,N):
-    if(i%2 == 0):
-        list_01.append(X[i])
-        list_02.append(Y[i])
-    elif(i%2 == 1):
-        list_01.append(Y[i])
-        list_02.append(X[i])
+# Loop to read input values
+for i in range(n):
+    X, Y = [int(e) for e in input().split()]
+    if i % 2 == 0:
+        A += [X]
+        B += [Y]
+    else:
+        A += [Y]
+        B += [X]
 
-# Output
-# Zig-Zag: Minimum of List 01 and Maximum of List 02
-# Zag-Zig: Minimum of List 02 and Maximum of List 01
-func = str(input())
-if(func == "Zig-Zag"):
-    print(min(list_01),max(list_02))
-elif(func == "Zag-Zig"):
-    print(min(list_02),max(list_01))
+# Output the results
+cmd = input().strip()
+if cmd == "Zig-Zag":
+    print(min(A), max(B))
+elif cmd == "Zag-Zig":
+    print(min(B), max(A))
