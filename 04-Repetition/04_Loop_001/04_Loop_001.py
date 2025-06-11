88e3263a41f2f070b7e147de8dbf97ef937b4dbb
reisenx <bloomrock555@gmail.com>
2025-06-11 19:09:15 +0700

    fix: formatting 04_loop_001 solution

--- 04-Repetition/04_Loop_001/04_Loop_001.py
+++ 04-Repetition/04_Loop_001/04_Loop_001.py
@@ -1,15 +1,19 @@
-# Input p as a float
+# --------------------------------------------------
+# File Name : 04_Loop_001.py
+# Problem   : Birthday Paradox (Flowchart)
+# Author    : Worralop Srichainont
+# Date      : 2025-06-11
+# --------------------------------------------------
+
 p = float(input())
 
-# Setup the value of k and t
 k = 1
 t = 1
-t = (t*(365-(k-1)))/365
 
-# Loop the process until 1-t >= p
-while(1-t < p):
-    k = k+1
-    t = (t*(365-(k-1)))/365
+t = (t * (365 - k + 1)) / 365
 
-# Output the value of k if 1-t >=p (loop process done)
+while t > 1 - p:
+    k += 1
+    t = (t * (365 - k + 1)) / 365
+
 print(k)
