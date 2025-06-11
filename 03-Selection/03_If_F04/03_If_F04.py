c1d512bc0c43deec8df31c3a9beb740bb2ba0276
reisenx <bloomrock555@gmail.com>
2025-06-11 15:12:29 +0700

    fix: formatting 03_if_F04 solution

--- 03-Selection/03_If_F04/03_If_F04.py
+++ 03-Selection/03_If_F04/03_If_F04.py
@@ -1,15 +1,15 @@
-# Create is_mobile_number(number) function
-def is_mobile_number(number):
-    # Create a list that indicate mobile number
-    mobile_num = ["06","08","09"]
-    
-    # Mobile number Requirement
-    # 1.) The first 2 digits must be "06", "08" or "09"
-    # 2.) Mobile number must be 10-digit long
-    if(number[0:2] in mobile_num and len(number)==10):
-        return True
-    else:
-        return False
+# --------------------------------------------------
+# File Name : 03_If_F04.py
+# Problem   : Mobile Number (Function)
+# Author    : Worralop Srichainont
+# Date      : 2025-06-11
+# --------------------------------------------------
 
-# Execute the input string
+
+# This function checks if a given string is a valid mobile number.
+def is_mobile_number(number: str) -> bool:
+    return len(number) == 10 and number[:2] in ["06", "08", "09"]
+
+
+# Execute the input string as code
 exec(input())
