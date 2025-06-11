b909245aedf3866c20ca5bba130c8cdd2082c2b4
reisenx <bloomrock555@gmail.com>
2025-06-11 19:09:25 +0700

    fix: formatting 04_loop_002 solution

--- 04-Repetition/04_Loop_002/04_Loop_002.py
+++ 04-Repetition/04_Loop_002/04_Loop_002.py
@@ -1,26 +1,23 @@
-# Input string then split them and collect in d list
+# --------------------------------------------------
+# File Name : 04_Loop_002.py
+# Problem   : Partition (Flowchart)
+# Author    : Worralop Srichainont
+# Date      : 2025-06-11
+# --------------------------------------------------
+
 d = [int(e) for e in input().split()]
 
-# Set initial value
-# p is the rightmost data in the list d
-# n is the length of the list d
 p = d[-1]
-i = -1 ; j = 0
+i = -1
+j = 0
 n = len(d)
 
-# Loop if j < n-1
-while(j < n-1):
-    # Condition: d[j] <= p
-    # If true, add i by 1 then swap the value d[i] and d[j]
-    # If false, do nothing
-    if(d[j] <= p):
+while j < n - 1:
+    if d[j] <= p:
         i += 1
-        d[i],d[j] = d[j],d[i]
-    # Add j by 1
+        d[i], d[j] = d[j], d[i]
     j += 1
 
-# Swap the value of d[i+1] and the rightmost data in list d
-d[i+1],d[-1] = d[-1],d[i+1]
+d[i + 1], d[-1] = d[-1], d[i + 1]
 
-# Output
 print(d)
