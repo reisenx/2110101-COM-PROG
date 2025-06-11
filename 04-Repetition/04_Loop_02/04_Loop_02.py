d6a1732f40d8c0d1cecc779c614e7fe0fbf71423
reisenx <bloomrock555@gmail.com>
2025-06-11 19:09:38 +0700

    fix: formatting 04_loop_02 solution

--- 04-Repetition/04_Loop_02/04_Loop_02.py
+++ 04-Repetition/04_Loop_02/04_Loop_02.py
@@ -1,23 +1,25 @@
-# Input a float
+# --------------------------------------------------
+# File Name : 04_Loop_02.py
+# Problem   : Bisection Log 10
+# Author    : Worralop Srichainont
+# Date      : 2025-06-11
+# --------------------------------------------------
+
+# Input a number
 a = float(input())
 
-# Create an interval [L,U] and x is the center of the interval
+# Initialize variables
 L = 0
 U = a
-x = (L+U)/2
+x = (L + U) / 2
 
-# Using bisection algorithm until |a-10^x| <= 10^(-10) * max(a,10^x)
-# Math Function
-# x = log10(a) --> 10^x = a
-# Algorithm
-# - If 10^x > a then change U = x --> interval [L,x]
-# - If 10^x < a then change L = x --> interval [x,U]
-while(abs(a-(10**x)) > (10**(-10)) * max(a,10**x)):
-    if(10**x > a):
+# Bisection method
+while abs(a - 10**x) > (10**-10) * max(a, 10**x):
+    if 10**x > a:
         U = x
-    elif(10**x < a):
+    else:
         L = x
-    x = (L+U)/2
+    x = (L + U) / 2
 
-# Output the value
-print(round(x,6))
+# Output the result
+print(round(x, 6))
