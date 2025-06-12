becd41b6502d56f442405a15159b79cc04d7828e
reisenx <bloomrock555@gmail.com>
2025-06-12 11:43:10 +0700

    fix: formatting 05_list_21 solution

--- 05-List-Processing/05_List_21/05_List_21.py
+++ 05-List-Processing/05_List_21/05_List_21.py
@@ -1,26 +1,33 @@
-# Input ID and grade of each student
-# Put the data in a 'students' list in the format
-# [[ID01,grade01], [ID02,grade02], ...]
+# --------------------------------------------------
+# File Name : 05_List_21.py
+# Problem   : Upgrade
+# Author    : Worralop Srichainont
+# Date      : 2025-06-11
+# --------------------------------------------------
+
+# List of letter grades
+GRADES = ["A", "B+", "B", "C+", "C", "D+", "D", "F"]
+
+# Input student information
 students = []
-while(True):
-    data = input().strip().split()
-    if(data[0] != "q"):
-        students.append(data)
-    else:
+while True:
+    student = input().strip()
+    # Stop if input is 'q'
+    if student == "q":
         break
+    # Add student information to the list
+    # Convert grade to index
+    id, grade = student.split()
+    students.append([id, GRADES.index(grade)])
 
-# Input ID that need to upgrade
-upgrade_ID = input().split()
+# Upgrade student grades
+upgrade_ids = input().split()
+for student in students:
+    id, grade = student
+    if id in upgrade_ids:
+        # Upgrade the grade of that student
+        student[1] = max(grade - 1, 0)
 
-# Upgrade B+ to A, B to B+, C+ to B and so on
-old_grade = ["A", "B+", "B", "C+", "C", "D+", "D", "F"]
-new_grade = ["A", "A", "B+", "B", "C+", "C", "D+", "D"]
-for details in students:
-    ID, grade = details[0], details[1]
-    if(ID in upgrade_ID):
-        index = old_grade.index(grade)
-        details[1] = new_grade[index]
-
-# Output
-for ID, grade in students:
-    print(ID,grade)
+# Output the upgraded grades
+for id, grade in students:
+    print(id, GRADES[grade])
