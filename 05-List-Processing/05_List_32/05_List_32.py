e251419a3f1bb04e627ef1087386498fa97e52d7
reisenx <bloomrock555@gmail.com>
2025-06-12 11:43:45 +0700

    fix: formatting 05_list_32 solution

--- 05-List-Processing/05_List_32/05_List_32.py
+++ 05-List-Processing/05_List_32/05_List_32.py
@@ -1,65 +1,59 @@
-# Input number of command lines
+# --------------------------------------------------
+# File Name : 05_List_32.py
+# Problem   : Queue Ticket
+# Author    : Worralop Srichainont
+# Date      : 2025-06-12
+# --------------------------------------------------
+
+# Input commands amount
 n = int(input())
 
-# Create empty lists and variable
-tickets = []
-new_time = []
-order_time = []
+# Initialize variable
+queue = []
 queue_time = []
-order_count = 0
+ticket = 0
+customer = 0
 
-# Input command lines
-for i in range(n):
-    command = input().split()
-    
-    # Command: 'reset'
-    # Set initial ticket number
-    # Example: reset 301 --> 'initial_ticket' = 301
-    # No output in this command
-    if(command[0] == "reset"):
-        initial_ticket = int(command[1])
-    
-    # Command: 'new'
-    # Create new ticket
-    # Example:
-    # > "new 1100" --> tickets = [301]      | new_time = [1100]
-    # > "new 1120" --> tickets = [301,302]  | new_time = [1100, 1120]
-    # NOTE: 1100 is a time when the ticket is created (time is an integer, not a HHMM format)
-    
-    # Output "ticket [number]" after create a ticket
-    # Example:
-    # > Input: "new 1100" --> Output: "ticket 301"
-    # > Input: "new 1120" --> Output: "ticket 302"
-    elif(command[0] == "new"):
-        if(len(tickets) == 0):
-            tickets.append(initial_ticket)
-        else:
-            tickets.append(tickets[-1] + 1)
-        new_time.append(int(command[1]))
-        print("ticket", tickets[-1])
-    
-    # Command: 'next'
-    # Call next queue
-    # Example:
-    # > Input: "next" --> Output: "call 301"  | tickets = [301,302]
-    elif(command[0] == "next"):
-        print("call", tickets[order_count])
-        order_count += 1
-    
-    # Command: 'order'
-    # Get time when calling a customer and calculate queue time
-    # Example: 
-    # > Input: "order 1130" --> Output: "qtime 301 30"
-    elif(command[0] == "order"):
-        order_count -= 1
-        order_time.append(int(command[1]))
-        queue_time.append(order_time[-1] - new_time[order_count])
-        print("qtime", tickets[order_count], queue_time[-1])
-        order_count += 1
-    
-    # Command: 'avg_qtime'
-    # Calculate average queue time up to the current customer
-    # Example:
-    # > Input: "avq_qtime" --> Output: "avq_qtime 25.0"
-    elif(command[0] == "avg_qtime"):
-        print("avg_qtime", float(sum(queue_time)/len(queue_time)))
+# Process each command
+for _ in range(n):
+    # Input command
+    cmd = input().strip().split()
+
+    # Command: reset
+    # Initialize ticket number
+    if cmd[0] == "reset":
+        ticket = int(cmd[1])
+
+    # Command: new
+    # Add a new customer to the queue
+    if cmd[0] == "new":
+        new_time = int(cmd[1])
+        queue.append([ticket, new_time])
+
+        print("ticket", ticket)
+        ticket += 1
+
+    # Command: next
+    # Process the next customer in the queue
+    if cmd[0] == "next":
+        current_ticket = queue[customer][0]
+        print("call", current_ticket)
+        customer += 1
+
+    # Command: order
+    # Update the order time for the current customer
+    if cmd[0] == "order":
+        current_ticket, new_time = queue[customer - 1]
+        order_time = int(cmd[1])
+
+        duration = order_time - new_time
+        queue_time.append(duration)
+        print("qtime", current_ticket, duration)
+
+    # Command: avg_qtime
+    # Calculate the average queue time
+    if cmd[0] == "avg_qtime":
+        avg = 0.0
+        if len(queue_time) > 0:
+            avg = sum(queue_time) / len(queue_time)
+        print("avg_qtime", round(avg, 4))
