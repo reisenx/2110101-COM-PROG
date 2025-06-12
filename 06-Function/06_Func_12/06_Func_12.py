44f1af3ba371044886e6370eb49d14572c9ca686
reisenx <bloomrock555@gmail.com>
2025-06-12 14:47:24 +0700

    fix: formatting 06_func_12 solution

--- 06-Function/06_Func_12/06_Func_12.py
+++ 06-Function/06_Func_12/06_Func_12.py
@@ -1,30 +1,39 @@
-# Create a function is_prime(n)
-# This function can check if the input number is a prime number
-def is_prime(n):
-    if n<=1:
-        return False
-    for k in range(2,int(n**0.5)+1):
-        if(n%k == 0):
-            return False
-    return True    
+# --------------------------------------------------
+# File Name : 06_Func_12.py
+# Problem   : Next Prime
+# Author    : Worralop Srichainont
+# Date      : 2025-06-12
+# --------------------------------------------------
 
-# Create a function next_prime(N)
-# This function returns a minimum prime number that greater than N
+
+# Check if a number is prime
+def is_prime(n):
+    if n <= 1:
+        return False
+    for k in range(2, int(n**0.5) + 1):
+        if n % k == 0:
+            return False
+    return True
+
+
+# Find the next prime number after N
 def next_prime(N):
-    # Set an initial number that greater than N
     N += 1
-    # Increase a value by 1 until N is a prime number
-    while(not is_prime(N)):
+    while not is_prime(N):
         N += 1
     return N
 
-# Create a function next_twin_prime(N)
-# This function return a minimum twin prime that greater than N
-# Twin prime is 2 prime numbers that differ by 2
-def next_twin_prime(N):
-    while(next_prime(next_prime(N)) - next_prime(N) != 2):
-        N = next_prime(N)
-    return (next_prime(N), next_prime(next_prime(N)))
 
-# Execute a input string
+# Find the next twin prime pair after N
+# Twin primes are pairs of prime numbers that differ by 2
+def next_twin_prime(N):
+    prime01 = next_prime(N)
+    prime02 = next_prime(prime01)
+    while prime02 - prime01 != 2:
+        prime01 = prime02
+        prime02 = next_prime(prime01)
+    return prime01, prime02
+
+
+# Execute a input string as code
 exec(input().strip())
