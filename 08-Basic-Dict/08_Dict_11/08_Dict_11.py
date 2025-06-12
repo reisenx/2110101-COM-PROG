bc92a6ab92cd6c8751d393a04b6a33f6e53d8d35
reisenx <bloomrock555@gmail.com>
2025-06-12 18:48:49 +0700

    fix: formatting 08_dict_11 solution

--- 08-Basic-Dict/08_Dict_11/08_Dict_11.py
+++ 08-Basic-Dict/08_Dict_11/08_Dict_11.py
@@ -1,17 +1,27 @@
-# This function can return a dictionary that swap keys and values
-def reverse(dict):
+# --------------------------------------------------
+# File Name : 08_Dict_11.py
+# Problem   : Reverse and Keys
+# Author    : Worralop Srichainont
+# Date      : 2025-06-12
+# --------------------------------------------------
+
+
+# Reverses the keys and values of a dictionary
+def reverse(dict: dict) -> dict:
     new_dict = {}
-    for key,value in dict.items():
+    for key, value in dict.items():
         new_dict[value] = key
     return new_dict
 
-# This function can a list that contains a key that have the input value
-def keys(dict,v):
-    keys_list = []
-    for key,value in dict.items():
-        if(value == v):
-            keys_list.append(key)
-    return keys_list
+
+# Returns a list of keys that have the specified value in the dictionary
+def keys(dict: dict, v: int) -> list:
+    keys = []
+    for key, value in dict.items():
+        if value == v:
+            keys.append(key)
+    return keys
+
 
 # Execute the input string
 exec(input().strip())
