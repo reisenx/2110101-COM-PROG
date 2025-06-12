3099c8b732d7c3b2d8127e254dc72f9491ac4a1e
reisenx <bloomrock555@gmail.com>
2025-06-12 17:08:41 +0700

    fix: formatting 07_strfile_21 solution

--- 07-String-Processing/07_StrFile_21/07_StrFile_21.py
+++ 07-String-Processing/07_StrFile_21/07_StrFile_21.py
@@ -1,46 +1,39 @@
-# Input an text and encode with ROT13 until the word "end"
+# --------------------------------------------------
+# File Name : 07_StrFile_21.py
+# Problem   : ROT13
+# Author    : Worralop Srichainont
+# Date      : 2025-06-12
+# --------------------------------------------------
 
-# Create a string with an alphabet
-lowercase = "abcdefghijklmnopqrstuvwxyz"
-uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# List of lowercase and uppercase letters
+LOWERCASE = "abcdefghijklmnopqrstuvwxyz"
+UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+# Function to encrypt text using ROT13 cipher
+def ROT13(text: str) -> str:
+    result = ""
+    for char in text:
+        # Encrypt lowercase letters
+        if char in LOWERCASE:
+            idx = LOWERCASE.index(char)
+            result += LOWERCASE[(idx + 13) % 26]
+        # Encrypt uppercase letters
+        elif char in UPPERCASE:
+            idx = UPPERCASE.index(char)
+            result += UPPERCASE[(idx + 13) % 26]
+        # Keep non-alphabetic characters unchanged
+        else:
+            result += char
+    return result
+
 
 # ROT13 encryption
-# Convert a letter in the first line into a second line
-# Convert a letter in the second line into a first line
-# A B C D E F G H I J K L M
-# | | | | | | | | | | | | |
-# N O P Q R S T U V W X Y Z
-ROT13_list = []
 while True:
-    text = input()
-    ROT13 = ""
-    if(text == "end"):
+    # Input a text
+    text = input().strip()
+    # Stop if the input is "end"
+    if text == "end":
         break
-    else:
-        for char in text:
-            # The character is in between a-m
-            if(char in lowercase[0:13]):
-                index = lowercase.find(char)
-                ROT13 += lowercase[index + 13]
-            # The character is in between n-z
-            elif(char in lowercase[13:26]):
-                index = lowercase.find(char)
-                ROT13 += lowercase[index - 13]
-            # The character is in between A-M
-            elif(char in uppercase[0:13]):
-                index = uppercase.find(char)
-                ROT13 += uppercase[index + 13]
-            # The character is in between N-Z
-            elif(char in uppercase[13:26]):
-                index = uppercase.find(char)
-                ROT13 += uppercase[index - 13]
-            # A character that is not an alphabet
-            else:
-                ROT13 += char
-            
-        # Put an encrypted text in a list
-        ROT13_list.append(ROT13)
-
-# Output
-for item in ROT13_list:
-    print(item)
+    # Output the encrypted text
+    print(ROT13(text))
