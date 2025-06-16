f61102c071363083983fe1ee634954e35b186487
reisenx <bloomrock555@gmail.com>
2025-06-16 19:35:05 +0700

    fix: formatting p1_flowchart_03 solution

--- P1-Grader-01-Practice/P1_Flowchart_03/P1_Flowchart_03.py
+++ P1-Grader-01-Practice/P1_Flowchart_03/P1_Flowchart_03.py
@@ -1,35 +1,47 @@
-# Import math library
+# --------------------------------------------------
+# File Name : P1_Flowchart_03.py
+# Problem   : Part-I Flowchart 03
+# Author    : Worralop Srichainont
+# Date      : 2025-06-16
+# --------------------------------------------------
+
 import math
 
-# Flowchart
 x = input().split()
-a,b,c,d = int(x[0]),int(x[1]),int(x[2]),int(x[3])
-if(a==1):
-    c,d = d,c
-    if(b == 1):
-        c = c+d
-    elif(b == 2):
-        c = c-d
-    elif(b != 3):
-        c = c+(c**d)
+a, b, c, d = int(x[0]), int(x[1]), int(x[2]), int(x[3])
+
+if a == 1:
+    c, d = d, c
+
+    if b == 1:
+        c = c + d
+    elif b == 2:
+        c = c - d
+    elif b != 3:
+        c = c + (c**d)
     else:
-        c=c/d
-    a = (d + math.sqrt((c/b)**3 + d))/(2 + b*d)
+        c = c / d
+
+    a = (d + math.sqrt((c / b) ** 3 + d)) / (2 + b * d)
+
 else:
-    if(a==2):
-        if(b>1):
-            c = c+d
-        if(b>2):
-            c = c/d
-        if(b>3):
-            c = c+(c**d)
+    if a == 2:
+        if b > 1:
+            c = c + d
+        if b > 2:
+            c = c / d
+        if b > 3:
+            c = c + (c**d)
         else:
-            a=math.log10(c)
+            a = math.log10(c)
+
     else:
-        while(a>d):
-            a = a-2
-            if(a<b):
+        while a > d:
+            a = a - 2
+
+            if a < b:
                 break
             else:
-                c = c+a
-print(a,b,c,d)
+                c = c + a
+
+print(a, b, c, d)
