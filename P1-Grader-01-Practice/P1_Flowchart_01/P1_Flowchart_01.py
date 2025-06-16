01c4726069d401bc47ada192c3ea972ad0f17235
reisenx <bloomrock555@gmail.com>
2025-06-16 19:34:58 +0700

    fix: formatting p1_flowchart_01 solution

--- P1-Grader-01-Practice/P1_Flowchart_01/P1_Flowchart_01.py
+++ P1-Grader-01-Practice/P1_Flowchart_01/P1_Flowchart_01.py
@@ -1,23 +1,29 @@
-# Flowchart
-x1,x2,x3,x4,x5 = [int(e) for e in input().split()]
-if(x1-x5 > x2):
-    if(x2 > x3+x1):
-        if(x3+x5 > x4):
+# --------------------------------------------------
+# File Name : P1_Flowchart_01.py
+# Problem   : Part-I Flowchart 01
+# Author    : Worralop Srichainont
+# Date      : 2025-06-16
+# --------------------------------------------------
+
+x1, x2, x3, x4, x5 = [int(e) for e in input().split()]
+
+if x1 - x5 > x2:
+    if x2 > x3 + x1:
+        if x3 + x5 > x4:
             print("C5")
         else:
-            if(x3 < x5):
+            if x3 < x5:
                 print("C6")
             else:
                 print("C7")
             print("C8")
-    else:
-        pass
+
 else:
-    if(x2-x1 > x3):
+    if x2 - x1 > x3:
         pass
     else:
-        if(x4 < x5+x1):
-            if(x3+x2 > x5):
+        if x4 < x5 + x1:
+            if x3 + x2 > x5:
                 print("C3")
             else:
                 print("C2")
