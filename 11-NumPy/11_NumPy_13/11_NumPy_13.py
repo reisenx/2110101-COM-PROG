15a38706e87aa7c0483665129859d47c3313e7e9
reisenx <bloomrock555@gmail.com>
2025-06-16 14:56:49 +0700

    fix: formatting 11_numpy_13 solution

--- 11-NumPy/11_NumPy_13/11_NumPy_13.py
+++ 11-NumPy/11_NumPy_13/11_NumPy_13.py
@@ -1,17 +1,18 @@
+# --------------------------------------------------
+# File Name : 11_Numpy_13.py
+# Problem   : Logistic Regression
+# Author    : Worralop Srichainont
+# Date      : 2025-06-16
+# --------------------------------------------------
+
 import numpy as np
-import math
 
-# == Formula ==
-# p(x) = 1/(1+e^(-logit(x)))
-# logit(x) = -3.98 + 0.1x0 + 0.5x1
 
-# X is an 2D array (nx2 size) contains [[x0_1 x1_1] [x0_2 x1_2] [x0_3 x1_3] ... [x0_n x1_n]]
-# X[:,0] = [x0_1 x0_2 x0_3 ... x0_n]
-# X[:,1] = [x1_1 x1_2 x1_3 ... x1_n]
-def p(X):
-    logit = -3.98 + (0.1*X[:,0]) + (0.5*X[:,1])
-    p = 1/(1 + math.e**(-logit))
-    return p
+# Calculate the probability of a binary outcome using logistic regression
+def p(X: np.ndarray) -> np.ndarray:
+    logit = -3.98 + (0.1 * X[:, 0]) + (0.5 * X[:, 1])
+    return 1 / (1 + np.exp(-logit))
 
-# Execute an input string
+
+# Execute an input string as code
 exec(input().strip())
