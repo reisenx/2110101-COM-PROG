063c28e82bf91c18b19a92336cd5b6e9d69b4842
reisenx <bloomrock555@gmail.com>
2025-06-16 12:59:58 +0700

    fix: formatting 10_tsd_35 solution

--- 10-Tuple-Set-Dict/10_TSD_35/10_TSD_35.py
+++ 10-Tuple-Set-Dict/10_TSD_35/10_TSD_35.py
@@ -1,36 +1,80 @@
-# Input student data
-# Example: [['Krit','A','97','CP'], ['Oat','A','98','CE'], ['Pim','C','99','CP'], ...]
+# --------------------------------------------------
+# File Name : 10_TSD_35.py
+# Problem   : Student Info
+# Author    : Worralop Srichainont
+# Date      : 2025-06-16
+# --------------------------------------------------
+
+# List of groups and departments
+GROUPS = (
+    "A",
+    "B",
+    "C",
+    "Dog",
+    "E",
+    "F",
+    "G",
+    "H",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+)
+DEPARTMENTS = (
+    "CE",
+    "EE",
+    "ME",
+    "AE",
+    "IE",
+    "CHE",
+    "PE",
+    "GE",
+    "ENV",
+    "SV",
+    "MT",
+    "CP",
+    "NT",
+    "CEDT",
+    "ICE",
+    "NANO",
+    "ADME",
+    "CHPE",
+    "AI",
+    "AERO",
+    "SEMI",
+)
+
+# Input student information
 n = int(input())
 students = []
-for i in range(n):
+for _ in range(n):
     data = input().strip().split()
     students.append(data)
 
-# Input search data as a set
-# Example: {'A','CP'}
-search = input().strip().split()
-search = set(search)
+# Input query as a set
+query = {input().strip().split()}
 
-# ---------- Search student ----------
-# 1.) Convert student to a set (Ex. ['Krit','A','97','CP'] --> {'Krit','A','97','CP'})
-# 2.) Check if the student has all attributes by using .issubset()
-#     {'A','CP'} is subset of {'Krit','A','97','CP'} --> Add to found
-#     {'A','CP'} is not subset of {'Oat','A','98','CE'} --> Skip
-# NOTE: There's a case like "Dog N 110 PE" or "ENV S 105 ME"
-found = []
-groups = ('A','B','C','Dog','E','F','G','H','J','K','L','M','N','P','Q','R','S','T')
-departments = ('CE','EE','ME','AE','IE','CHE','PE','GE','ENV','SV','MT','CP','NT','CEDT','ICE','NANO','ADME','CHPE','AI','AERO','SEMI')
-
+# Search for students matching the query
+results = []
 for student in students:
     name, group, generation, department = student
-    if(search.issubset(set(student)) and (name not in groups) and (name not in departments)):
-        found.append(student)
+    if (
+        query.issubset(set(student))
+        and (name not in GROUPS)
+        and (name not in DEPARTMENTS)
+    ):
+        results.append(student)
 
 # Output
-if(len(found) == 0):
+if len(results) == 0:
     print("Not Found")
 else:
-    # Don't forget to sort students by name
-    found.sort()
-    for name, group, generation, department in found:
+    results.sort()
+    for name, group, generation, department in results:
         print(name, group, generation, department)
