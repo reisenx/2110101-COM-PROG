e922595189021a9ee4e1242e22af3303dbd864f5
reisenx <bloomrock555@gmail.com>
2025-06-16 14:56:41 +0700

    fix: formatting 11_numpy_12 solution

--- 11-NumPy/11_NumPy_12/11_NumPy_12.py
+++ 11-NumPy/11_NumPy_12/11_NumPy_12.py
@@ -1,32 +1,31 @@
+# --------------------------------------------------
+# File Name : 11_Numpy_12.py
+# Problem   : Scalar and Array
+# Author    : Worralop Srichainont
+# Date      : 2025-06-16
+# --------------------------------------------------
+
 import numpy as np
 
-# This function can convert temperature in farenheit to celsius
-# F is an 1D array that contains temperature in fahrenheit
-# C = (F-32)/1.8
-def toCelsius(F):
-    celsius = (F-32)/1.8
-    return celsius
 
-# This function can calculate BMI from input array
-# wh is an 2D array (nx2) that contains [[w1 h1] [w2 h2] ...] (weight in kg and height in cm) 
-# So wh[:,0] is vector of weight and w[:,1] is vector of height
-# Example: wh = [[60 170] [50 160]] --> wh[:,0] = [60 50] and wh[:,1] = [170 160]
-# BMI = weight/(height^2) <-- height in m
-def BMI(wh):
-    weight = wh[:,0]
-    height = wh[:,1] / 100
+# Convert Fahrenheit to Celsius
+def toCelsius(F: np.ndarray) -> np.ndarray:
+    return (F - 32) / 1.8
+
+
+# Calculate Body Mass Index (BMI)
+def BMI(weight_and_height: np.ndarray) -> np.ndarray:
+    weight = weight_and_height[:, 0]
+    height = weight_and_height[:, 1] / 100
     return weight / (height**2)
 
-# This function can calculate distance from 'p' to each coordinate in 'Points'
-# p is an 1D array that contains [x0 y0] --> p[0] = x0 and p[1] = y0
-# Points is an 2D array that contains [[x1 y1] [x2 y2] ...]
-# > Points[:,0] is vector of x-coordinate = [x1 x2 x3 ...]
-# > Points[:,1] is vector of y-coordinate = [y1 y1 y2 ...]
-# Example: p = [0 0] and Points [[3 0] [0 4] [3 4]] returns [3. 4. 5.]
-def distanceTo(p, Points):
-    distance_x = Points[:,0] - p[0]
-    distance_y = Points[:,1] - p[1]
-    return ((distance_x**2) + (distance_y**2))**0.5
 
-# Execute an input string
+# Calculate the distance from a point to all points in an array
+def distanceTo(point: np.ndarray, all_points: np.ndarray) -> np.ndarray:
+    distance_x = all_points[:, 0] - point[0]
+    distance_y = all_points[:, 1] - point[1]
+    return ((distance_x**2) + (distance_y**2)) ** 0.5
+
+
+# Execute an input string as code
 exec(input().strip())
