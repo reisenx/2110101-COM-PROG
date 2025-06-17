81e1a5344101294e6c4e10c8e37aad74d2aad80b
reisenx <bloomrock555@gmail.com>
2025-06-17 14:07:52 +0700

    fix: formatting p2_07_snooker solution

--- P2-Grader-02-Practice/P2_07_Snooker/P2_07_Snooker.py
+++ P2-Grader-02-Practice/P2_07_Snooker/P2_07_Snooker.py
@@ -1,31 +1,48 @@
-# Create a dictionary that contains a score in each case
-score_dict = {'X':0, 'Y':2, 'G':3, 'W':4, 'B':5, 'P':6, 'K':7,
-              'RX':1, 'RY':3, 'RG':4, 'RW':5, 'RB':6, 'RP':7, 'RK':8}
+# --------------------------------------------------
+# File Name : P2_07_Snooker.py
+# Problem   : Part-II Snooker
+# Author    : Worralop Srichainont
+# Date      : 2025-06-17
+# --------------------------------------------------
 
-# Calculate each player score
-# Example input: "1RK" means player 1 got 8 points (RK) that round
-# The game ends when a player shoot a black ball (1K or 2K)
-player01 = 0
-player02 = 0
+# Initialize the score mapping for snooker balls
+SCORES = {
+    "X": 0,
+    "Y": 2,
+    "G": 3,
+    "W": 4,
+    "B": 5,
+    "P": 6,
+    "K": 7,
+    "RX": 1,
+    "RY": 3,
+    "RG": 4,
+    "RW": 5,
+    "RB": 6,
+    "RP": 7,
+    "RK": 8,
+}
+
+# Initialize player scores
+player_scores = [0, 0]
+
+# Both players start the game
 while True:
-    round = input().strip()
-    if(round == '1K' or round == '2K'):
-        if(round[0] == '1'):
-            player01 += score_dict['K']
-        elif(round[0] == '2'):
-            player02 += score_dict['K']
+    # Read input data
+    data = input().strip()
+    player = int(data[0]) - 1
+    ball = data[1:]
+    # Update the score for the current player
+    player_scores[player] += SCORES[ball]
+    # The game ends when a player pots the black ball
+    if ball == "K":
         break
-    else:
-        if(round[0] == '1'):
-            player01 += score_dict[round[1:]]
-        elif(round[0] == '2'):
-            player02 += score_dict[round[1:]]
 
-# Output
-print(player01, player02)
-if(player01 == player02):
+# Output the scores and the winner
+print(player_scores[0], player_scores[1])
+if player_scores[0] == player_scores[1]:
     print("Tie")
-elif(player01 > player02):
+elif player_scores[0] > player_scores[1]:
     print("Player 1 wins")
-elif(player02 > player01):
+else:
     print("Player 2 wins")
