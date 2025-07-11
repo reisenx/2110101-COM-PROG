4f142fc21a3d997f7e9016c69b4cf09b9c90ae03
Worralop Srichainont <worralop.sric@gmail.com>
2025-07-11 17:50:35 +0700

    fix: formatting 2565_1_q1_03 solution

--- GE-Grader-Examination/G6501-Exam-2565-S1/Grader-01/2565_1_Q1_03/2565_1_Q1_03.py
+++ GE-Grader-Examination/G6501-Exam-2565-S1/Grader-01/2565_1_Q1_03/2565_1_Q1_03.py
@@ -1,31 +1,21 @@
-# Not guarantee 100/100 points on this code
+# --------------------------------------------------
+# File Name : 2565_1_Q1_03.py
+# Problem   : Simple Expression Calculator
+# Author    : Worralop Srichainont
+# Date      : 2025-07-11
+# --------------------------------------------------
 
-# Reminder
-# 1.) Import a library is prohibited
-# 2.) eval() and exec() command is also prohibited
+# Input equation and add spaces before operators
+equation = ""
+for char in input().split():
+    if char in "+-":
+        equation += " "
+    equation += char
 
-# The task is simple, just fine the answer of the input string
-# The input won't have weird cases like "1+2-3.0", "1+2/3", "one+two", "1  +      2     -3"
-# Example: "1+2-3+4-100+500"
+# Split the equation string into numbers
+numbers = []
+for num in equation.split():
+    numbers += [int(num)]
 
-# Input a string
-equation = input().strip()
-
-# Delete "+", then add a spacebar between number then split to a list of number
-# Example: "1+2-3+4-100+500" --> "1 2 -3 4 -100 500" --> [1,2,-3,4,-100,500]
-number = ""
-for char in equation:
-    if(char == '+'):
-        number += " "
-    elif(char == "-"):
-        number += " -"
-    else:
-        number += char
-number = number.split()
-# Convert from string to integer
-# ['1','2','-3','4','-100','500'] --> [1,2,-3,4,-100,500]
-for i in range(len(number)):
-    number[i] = int(number[i])
-
-# Output the sum of number in a list
-print(sum(number))
+# Calculate the sum of the numbers and print the result
+print(sum(numbers))
