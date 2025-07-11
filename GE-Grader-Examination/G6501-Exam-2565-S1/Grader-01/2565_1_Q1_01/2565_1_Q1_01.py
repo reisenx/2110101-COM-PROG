7ef4deb57700165b1e4e24782de1274d118e5f5f
Worralop Srichainont <worralop.sric@gmail.com>
2025-07-11 17:50:12 +0700

    fix: formatting 2565_1_q1_01 solution

--- GE-Grader-Examination/G6501-Exam-2565-S1/Grader-01/2565_1_Q1_01/2565_1_Q1_01.py
+++ GE-Grader-Examination/G6501-Exam-2565-S1/Grader-01/2565_1_Q1_01/2565_1_Q1_01.py
@@ -1,21 +1,25 @@
-# Flowchart
-# Not guarantee 100/100 points on this code
+# --------------------------------------------------
+# File Name : 2565_1_Q1_01.py
+# Problem   : Flowchart
+# Author    : Worralop Srichainont
+# Date      : 2025-07-11
+# --------------------------------------------------
 
-a,b,c,d = [int(e) for e in input().split()]
-if(a > b):
-    a,b = b,a
-    while(d >= a):
-        if(c > d):
+a, b, c, d = [int(e) for e in input().split()]
+if a > b:
+    a, b = b, a
+    while d >= a:
+        if c > d:
             a += 1
         else:
             d -= 1
 else:
-    if(c%2 == 0):
-        d = d+a
+    if c % 2 == 0:
+        d = d + a
     else:
-        if(d > c):
-            c = c+d
+        if d > c:
+            c = c + d
         else:
-            b = b+a
-        a = b+c
-print(a,b,c,d)
+            b = b + a
+        a = b + c
+print(a, b, c, d)
