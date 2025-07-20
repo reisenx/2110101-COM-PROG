45b2582ad40a433a3be2928809f360d391065635
Worralop Srichainont <worralop.sric@gmail.com>
2025-07-20 15:00:11 +0700

    feat: formatting 2566_1_q1_02 solution

--- GE-Grader-Examination/G6601-Exam-2566-S1/Grader-02/2566_1_Q2_02/2566_1_Q2_02.py
+++ GE-Grader-Examination/G6601-Exam-2566-S1/Grader-02/2566_1_Q2_02/2566_1_Q2_02.py
@@ -1,60 +1,49 @@
-# Not guarantee 100/100 points on this code
-# Example testcase filename: 2110100.txt
+# --------------------------------------------------
+# File Name : 2566_1_Q2_02.py
+# Problem   : Registration
+# Author    : Worralop Srichainont
+# Date      : 2025-07-12
+# --------------------------------------------------
 
-# Input filename
+# Input course database filename and query professor name
 filename = input().strip()
+search_professor = input().strip()
 
-# Open a file
-file = open(filename, "r")
+# Initialize lists to store information of query professor
+sections = []
+males = []
+females = []
 
-# Read student details in a file
-# Each line in the file will be in this format
-# [ID],[Gender],[Faculty code],[Section],[Professor]
-# We will read and collect the data in the dictionary in this format
-# professor = {Professor:[[List of sections], Female, Male]}
-professor = {}
-for line in file:
-    ID,gender,faculty,sec,prof = line.strip().split(',')
-    sec = int(sec)
-    # Add a new data, if that professor data is not exists yet
-    if(prof not in professor):
-        if(gender == 'F'):
-            professor[prof] = [[sec],1,0]
-        elif(gender == 'M'):
-            professor[prof] = [[sec],0,1]
-    # Edit data, if that professor data is already exists
-    else:
-        # Check gender
-        if(gender == 'F'):
-            professor[prof][1] += 1
-        elif(gender == 'M'):
-            professor[prof][2] += 1
-        # Check section
-        if(sec not in professor[prof][0]):
-            professor[prof][0].append(sec)
+# Read the file and get information of the query professor
+with open(filename) as file:
+    for line in file:
+        # Extract data from each line
+        sid, sex, faculty, section, professor = line.strip().split(",")
+        # If the professor matches the search query, store the information
+        if professor == search_professor:
+            # Store section number of the query professor
+            if int(section) not in sections:
+                sections.append(int(section))
+            # Store male student IDs of the query professor
+            if sex == "M" and sid not in males:
+                males.append(sid)
+            # Store female student IDs of the query professor
+            elif sex == "F" and sid not in females:
+                females.append(sid)
+# Sort the sections in ascending order
+sections.sort()
 
-# Sort the section list
-for prof in professor:
-    professor[prof][0].sort()
+# Determine the output based on the number of sections found
+result = ""
+if len(sections) == 0:
+    result = "Not found"
+elif len(sections) == 1:
+    result = f"Section: {sections[0]}"
+elif len(sections) > 1:
+    result = f"Sections: {','.join([str(section) for section in sections])}"
 
-# Close the file
-file.close()
-
-# Input name of the professor
-output_prof = input().strip()
-
+# Add the student count to the result if there are sections found
+if len(sections) > 0:
+    result += f" --> F = {len(females)}, M = {len(males)}"
 # Output
-if(output_prof not in professor):
-    print("Not found")
-else:
-    text = ""
-    sec_list,M,F = professor[output_prof]
-    for i in range(len(sec_list)):
-        sec_list[i] = str(sec_list[i])
-    # Put 's' after 'Section' when that professor teach more than 1 section
-    if(len(sec_list) == 1):
-        text += "Section: "
-    elif(len(sec_list) > 1):
-        text += "Sections: "
-    text += ",".join(sec_list) + " --> " + "F = " + str(F) + ", M = " + str(M)
-    print(text)
+print(result)
