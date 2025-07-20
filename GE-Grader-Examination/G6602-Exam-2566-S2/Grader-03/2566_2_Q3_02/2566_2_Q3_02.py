fd9176af08d6656dd0871d2705389b9d876d21c8
Worralop Srichainont <worralop.sric@gmail.com>
2025-07-20 15:38:14 +0700

    feat: formatting 2566_2_q3_02 solution

--- GE-Grader-Examination/G6602-Exam-2566-S2/Grader-03/2566_2_Q3_02/2566_2_Q3_02.py
+++ GE-Grader-Examination/G6602-Exam-2566-S2/Grader-03/2566_2_Q3_02/2566_2_Q3_02.py
@@ -1,34 +1,48 @@
-def read_friends():
-    dat = []
-    N = int(input())
-    for i in range(N):
-        dat.append(tuple(input().strip().split()))
-    return dat
+# --------------------------------------------------
+# File Name : 2566_2_Q3_01.py
+# Problem   : Friends
+# Author    : Worralop Srichainont
+# Date      : 2025-07-15
+# --------------------------------------------------
 
-# [('A', 'B'), ('C', 'D')]
-# Convert it to {'A':{'B'}, B:{'A'}, ...}
-def tuple_to_dict(data):
-    friends = {}
-    for name01,name02 in data:
-        if(name01 not in friends):
-            friends[name01] = {name02}
-        else:
-            friends[name01].add(name02)
-        if(name02 not in friends):
-            friends[name02] = {name01}
-        else:
-            friends[name02].add(name01)
+
+# Read relationship data and return a list of tuples.
+def read_friends() -> list[tuple[str, str]]:
+    friends = []
+    n = int(input())
+    for _ in range(n):
+        friends.append(tuple(input().strip().split()))
     return friends
-                
-def count_friends(data, names):
-    friends = tuple_to_dict(data)
-    count = []
-    for item in names:
-        if(item in friends):
-            count.append((item, len(friends[item])))
-        else:
-            count.append((item, 0))
-    count.sort()
-    return count
 
+
+# Count the number of friends for each person in the list of names.
+def count_friends(
+    friends: list[tuple[str, str]], names: list[str]
+) -> list[tuple[str, int]]:
+    # Create a dictionary to store set of friends of each person.
+    all_friends = {}
+    for a, b in friends:
+        # Initialize the sets for each person if they do not exist.
+        if a not in all_friends:
+            all_friends[a] = set()
+        if b not in all_friends:
+            all_friends[b] = set()
+        # Add each person to the other's set of friends.
+        all_friends[a].add(b)
+        all_friends[b].add(a)
+
+    # Sort the names and count the number of friends for each.
+    result = []
+    for name in sorted(names):
+        # If the name is not in the dictionary, they have no friends.
+        if name not in all_friends:
+            result.append((name, 0))
+        # Otherwise, append the name and the count of friends.
+        else:
+            result.append((name, len(all_friends[name])))
+    # Return the sorted list of tuples with names and their friend counts.
+    return result
+
+
+# Execute the input string as code
 exec(input().strip())
