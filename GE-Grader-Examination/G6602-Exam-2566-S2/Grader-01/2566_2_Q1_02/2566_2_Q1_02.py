e05881f56691a840c822170fb8499d20eb59b68b
Worralop Srichainont <worralop.sric@gmail.com>
2025-07-20 15:37:12 +0700

    feat: formatting 2566_2_q1_02 solution

--- GE-Grader-Examination/G6602-Exam-2566-S2/Grader-01/2566_2_Q1_02/2566_2_Q1_02.py
+++ GE-Grader-Examination/G6602-Exam-2566-S2/Grader-01/2566_2_Q1_02/2566_2_Q1_02.py
@@ -1,32 +1,39 @@
-n,a,b = [int(e) for e in input().split()]
+# --------------------------------------------------
+# File Name : 2566_2_Q1_02.py
+# Problem   : Flowchart
+# Author    : Worralop Srichainont
+# Date      : 2025-07-13
+# --------------------------------------------------
 
-if(a>b):
-    d,e = -1, -1
-    while(n<a):
+n, a, b = [int(e) for e in input().split()]
+
+if a > b:
+    d, e = -1, -1
+    while n < a:
         c = int(input())
-        if(c>d):
-            d,e = c,d
-        elif (c>e):
+        if c > d:
+            d, e = c, d
+        elif c > e:
             e = c
-        n = n+b
+        n = n + b
 else:
     c = int(input())
     d = int(input())
     e = int(input())
     f = int(input())
-    if(e<f):
-        e,f = f,e
-    if(d<e):
-        d,e = e,d
-    if(c<d):
-        c,d = d,c
-    if(d>e):
-        if(d>f):
+    if e < f:
+        e, f = f, e
+    if d < e:
+        d, e = e, d
+    if c < d:
+        c, d = d, c
+    if d > e:
+        if d > f:
             pass
         else:
-            d,f = f,d
-    elif(e>f):
-        d,e = e,d
+            d, f = f, d
+    elif e > f:
+        d, e = e, d
     else:
-        d,f = f,d
-print(d,e)
+        d, f = f, d
+print(d, e)
