b76e69a1af060d61ca0afb8d2f0b4b2eb9db2ca8
Worralop Srichainont <worralop.sric@gmail.com>
2025-07-29 14:01:40 +0700

    feat: formatting 2567_1_q3_b2-s solution

--- GE-Grader-Examination/G6701-Exam-2567-S1/Grader-03-Survey/2567_1_Q3_B2-S/2567_1_Q3_B2-S-sol2.py
+++ GE-Grader-Examination/G6701-Exam-2567-S1/Grader-03-Survey/2567_1_Q3_B2-S/2567_1_Q3_B2-S-sol2.py
@@ -1,31 +1,46 @@
-# Object Oriented Programming in Python
-# Create a class makes it a lot easier to sort them
+# --------------------------------------------------
+# File Name : 2567_1_Q3_B2-S-sol2.py
+# Problem   : Card Sorting
+# Author    : Worralop Srichainont
+# Date      : 2025-07-29
+# --------------------------------------------------
+
+# Initialize lists for each suit
+VALUE_EX = {"J": 11, "Q": 12, "K": 13, "A": 14}
+SUIT = {"S": 0, "C": 1, "D": 2, "H": 3}
+
+
+# Create a class to represent a card
 class Card:
-    def __init__(self, card):
-        val = {'J':11, 'Q':12, 'K':13, 'A':14}
-        suit = {'S':0, 'C':1, 'D':2, 'H':3}
-        if(card[0:-1] in val):
-            self.value = val[card[0:-1]]
-        else:
-            self.value = int(card[0:-1])
+    # Initialize the card with its name, value and suit
+    def __init__(self, card: str) -> None:
+        # Initialize card name
+        self.name = card
 
-        self.suit = suit[card[-1]]
-    
-    def __str__(self):
-        val = {11:'J', 12:'Q', 13:'K', 14:'A'}
-        suit = {0:'S', 1:'C', 2:'D', 3:'H'}
-        if(self.value in val):
-            return val[self.value] + suit[self.suit]
-        else:
-            return str(self.value) + suit[self.suit]
-    
-    def __lt__(self, rhs):
-        if(self.suit == rhs.suit):
-            return self.value < rhs.value
-        return self.suit < rhs.suit
+        # Initialize card suit by converting to integer
+        self.suit = SUIT[card[-1]]
 
-cards = input().strip().strip('|').split('|')
-cards = [Card(item) for item in cards]
-cards.sort()
-cards = [str(item) for item in cards]
-print('|' + '|'.join(cards) + '|')
+        # Initialize card value by converting to integer
+        if card[:-1] in VALUE_EX:
+            self.value = VALUE_EX[card[:-1]]
+            return
+        self.value = int(card[:-1])
+
+    # Convert card to string representation
+    def __str__(self) -> str:
+        return self.name
+
+    # Define less than operator for sorting by comparing suit and value
+    def __lt__(self, rhs: "Card") -> bool:
+        return (self.suit, self.value) < (rhs.suit, rhs.value)
+
+
+# Create a list of Card objects from the input and sort them in ascending order
+card_objects = [Card(card) for card in input().strip("| ").split("|")]
+card_objects.sort()
+
+# Convert sorted Card objects back to string representation
+cards = [str(card) for card in card_objects]
+
+# Output the sorted cards with '|' at the start and end
+print(f"|{'|'.join(cards)}|")
