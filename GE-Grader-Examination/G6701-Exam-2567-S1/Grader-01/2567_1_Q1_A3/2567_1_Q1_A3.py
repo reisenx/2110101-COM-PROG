d0f9b12058fa2ff3ed24f5d11ee27007426129d8
Worralop Srichainont <worralop.sric@gmail.com>
2025-07-29 13:49:59 +0700

    feat: formatting 2567_1_q1_a3 solution

--- GE-Grader-Examination/G6701-Exam-2567-S1/Grader-01/2567_1_Q1_A3/2567_1_Q1_A3.py
+++ GE-Grader-Examination/G6701-Exam-2567-S1/Grader-01/2567_1_Q1_A3/2567_1_Q1_A3.py
@@ -1,50 +1,36 @@
-# Input image as a list of string
-# Example: img = ["@@@@@.", "@@@@@.", "@@@@@.", "@.....", "@....."] can be represented as
-# @@@@@.
-# @...@.
-# @@@@@.
-# @.....
-# @.....
-row = int(input())
-img = []
-for i in range(row):
-    data = input().strip()
-    img.append(data)
+# --------------------------------------------------
+# File Name : 2567_1_Q1_A3.py
+# Problem   : rot90 & rot180
+# Author    : Worralop Srichainont
+# Date      : 2025-07-28
+# --------------------------------------------------
 
-# Find column of the picture
-col = len(img[0])
+# Input number of rows and the image
+rows = int(input())
+img = [input().strip() for _ in range(rows)]
 
-# Input command
-command = input()
-# Create newImg as an empty list to contains rotated image
-newImg = []
+# Find the number of columns
+cols = len(img[0])
 
-# Rotate 90 degree clockwise
-# Original Image        | Rotated Image
-# [00][01][02]...[0c]   | [r0]...[20][10][00]
-# [10][11][12]...[1c]   | [r1]...[21][11][01]
-# [20][21][22]...[2c]   | [r2]...[22][12][02]
-# ... ... ...    ...    | ...    ... ... ...
-# [r0][r1][r2]...[rc]   | [rc]...[2c][1c][0c]
-if(command == "rot90"):
-    for c in range(col):
-        data = ""
-        for r in range(row-1, -1, -1):
-            data += img[r][c]
-        newImg.append(data)
+# Initialize the modified image
+modified_img = []
 
-# Rotate 180 degree
-# Original Image        | Rotated Image
-# [00][01][02]...[0c]   | [rc]...[r2][r1][r0]
-# [10][11][12]...[1c]   | ...    ... ... ...
-# [20][21][22]...[2c]   | [2c]...[22][21][20]
-# ... ... ...    ...    | [1c]...[12][11][10]
-# [r0][r1][r2]...[rc]   | [0c]...[02][01][00]
-elif(command == "rot180"):
-    for r in range(row-1, -1, -1):
-        data = img[r][::-1]
-        newImg.append(data)
+# Input command to modify the image
+cmd = input().strip()
 
-# Output
-for txt in newImg:
-    print(txt)
+# Rotate the image by 90 degrees clockwise
+if cmd == "rot90":
+    for c in range(cols):
+        line = ""
+        for r in range(rows - 1, -1, -1):
+            line += img[r][c]
+        modified_img.append(line)
+
+# Rotate the image by 180 degrees
+elif cmd == "rot180":
+    for r in range(rows - 1, -1, -1):
+        modified_img.append(img[r][::-1])
+
+# Output the modified image
+for line in modified_img:
+    print(line)
