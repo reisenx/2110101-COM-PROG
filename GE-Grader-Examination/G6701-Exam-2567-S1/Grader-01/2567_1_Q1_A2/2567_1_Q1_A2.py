98fe206ad2f5b99573119ed47c6e7efd81dc9ae1
Worralop Srichainont <worralop.sric@gmail.com>
2025-07-29 13:49:54 +0700

    feat: formatting 2567_1_q1_a2 solution

--- GE-Grader-Examination/G6701-Exam-2567-S1/Grader-01/2567_1_Q1_A2/2567_1_Q1_A2.py
+++ GE-Grader-Examination/G6701-Exam-2567-S1/Grader-01/2567_1_Q1_A2/2567_1_Q1_A2.py
@@ -1,25 +1,39 @@
-# Input data
-birth, money, months, currentMonth = input().split()
-bd, bm, by = birth.split('/')
-bm, money, months, currentMonth = int(bm), float(money), int(months), int(currentMonth)
+# --------------------------------------------------
+# File Name : 2567_1_Q1_A2.py
+# Problem   : Interest Rate
+# Author    : Worralop Srichainont
+# Date      : 2025-07-28
+# --------------------------------------------------
 
-# Calculate money
-for n in range(1, months+1):
-    # Check interest rate
-    # n = 1,5,9,13,...  | n % 4 == 1 | interest = 0.01
-    # n = 2,6,10,14,... | n % 4 == 2 | interest = 0.02
-    # n = 3,7,11,15,... | n % 4 == 3 | interest = 0.03
-    # n = 4,8,12,16,... | n % 4 == 0 | interest = 0.04
-    if(n % 4 == 0):
-        interest = 0.04
-    else:
-        interest = (n % 4)/100
-    # Check birth month (Beware of bm = 12 case)
-    if(currentMonth % 12 == bm % 12):
-        interest += 0.01
-    # Calculate money
-    money += money * interest * (1/12)
-    currentMonth += 1
+# Initial interest rates for each month
+INITIAL_INTEREST_RATE = [0.04, 0.01, 0.02, 0.03]
 
-# Output
-print(round(money, 2))
+# Input
+data = input().split()
+
+# Get birth month, initial money, total months, and current month from input
+birth_month = int(data[0].split("/")[1])
+initial_money = float(data[1])
+total_months = int(data[2])
+current_month = int(data[3])
+
+# Initialize total money with the initial money
+total_money = initial_money
+
+# Loop through each month to calculate the interest
+for month in range(1, total_months + 1):
+    # Determine interest rate based on month
+    interest_rate = INITIAL_INTEREST_RATE[month % 4]
+
+    # Check if current month matches birth month
+    if (current_month % 12) == (birth_month % 12):
+        interest_rate += 0.01
+
+    # Calculate new total money amount
+    total_money += total_money * interest_rate * (1 / 12)
+
+    # Increment current month
+    current_month += 1
+
+# Output the total money rounded to two decimal places
+print(round(total_money, 2))
