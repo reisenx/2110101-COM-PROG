90654c80da158a9de8ed8b3b492db74b8917d41e
Worralop Srichainont <worralop.sric@gmail.com>
2025-07-29 13:49:47 +0700

    feat: formatting 2567_1_q1_a1 solution

--- GE-Grader-Examination/G6701-Exam-2567-S1/Grader-01/2567_1_Q1_A1/2567_1_Q1_A1.py
+++ GE-Grader-Examination/G6701-Exam-2567-S1/Grader-01/2567_1_Q1_A1/2567_1_Q1_A1.py
@@ -1,32 +1,37 @@
-# Flowchart
-a,b,c,d = [int(e) for e in input().split()]
-if(a == 1):
-    a = b + c + d
-    if(b == 1):
-        c += c+d
-    else:
-        if(b == 2):
-            c += c-d
-        else:
-            if(b > 4):
-                c += c*d
-            else:
-                pass
-            if(b > 5):
-                c += c%d
-            else:
-                c += c//d
-    print(c)
+# --------------------------------------------------
+# File Name : 2567_1_Q1_A1.py
+# Problem   : Flowchart
+# Author    : Worralop Srichainont
+# Date      : 2025-07-28
+# --------------------------------------------------
 
-else:
-    while(b < c):
-        b += 1
-        if(b > a):
-            break
+a, b, c, d = [int(e) for e in input().split()]
+
+if a == 1:
+    a = b + c + d
+    if b == 1:
+        c += c + d
+    else:
+        if b == 2:
+            c += c - d
         else:
-            c -= 1
-            if(a > d):
-                break
+            if b > 4:
+                c += c * d
+            if b > 5:
+                c += c % d
             else:
-                a += c
-print(a,b,c,d)
+                c += c // d
+    print(c)
+else:
+    while b < c:
+        b += 1
+        if b > a:
+            break
+
+        c -= 1
+
+        if a > d:
+            break
+        a += c
+
+print(a, b, c, d)
