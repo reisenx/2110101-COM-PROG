7e7b85db3f4a7de96def6d6911ba0f14d36f86b1
Worralop Srichainont <worralop.sric@gmail.com>
2025-07-29 13:51:02 +0700

    feat: formatting 2567_1_q2_a1 solution

--- GE-Grader-Examination/G6701-Exam-2567-S1/Grader-02/2567_1_Q2_A1/2567_1_Q2_A1.py
+++ GE-Grader-Examination/G6701-Exam-2567-S1/Grader-02/2567_1_Q2_A1/2567_1_Q2_A1.py
@@ -1,29 +1,35 @@
-# Flowchart
-a,b,c,d = [int(e) for e in input().split()]
-if(a == 1):
+# --------------------------------------------------
+# File Name : 2567_1_Q2_A1.py
+# Problem   : Flowchart
+# Author    : Worralop Srichainont
+# Date      : 2025-07-28
+# --------------------------------------------------
+
+a, b, c, d = [int(e) for e in input().split()]
+if a == 1:
     a = b + c + d
-    if(b > 5):
-        if(b > 9):
-            c = 4*d
+    if b > 5:
+        if b > 9:
+            c = 4 * d
         else:
-            if(b > 7):
-                c = 5*d
+            if b > 7:
+                c = 5 * d
             else:
-                c = 6*d
+                c = 6 * d
     else:
-        if(b > 3):
+        if b > 3:
             c = d
         else:
-            if(b > 0):
-                c = 2*d
+            if b > 0:
+                c = 2 * d
             else:
-                c = 3*d
+                c = 3 * d
 else:
-    while(b < c):
+    while b < c:
         b += 1
-        if(b > a):
+        if b > a:
             a += c
             c -= 1
         else:
             c -= 1
-print(a,b,c,d)
+print(a, b, c, d)
