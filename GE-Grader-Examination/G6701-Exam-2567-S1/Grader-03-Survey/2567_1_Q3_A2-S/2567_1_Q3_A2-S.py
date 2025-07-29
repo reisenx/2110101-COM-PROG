c76358aa741169bc46360702eb6c322a5deb0f49
Worralop Srichainont <worralop.sric@gmail.com>
2025-07-29 13:58:54 +0700

    feat: formatting 2567_1_q3_a2-s solution

--- GE-Grader-Examination/G6701-Exam-2567-S1/Grader-03-Survey/2567_1_Q3_A2-S/2567_1_Q3_A2-S.py
+++ GE-Grader-Examination/G6701-Exam-2567-S1/Grader-03-Survey/2567_1_Q3_A2-S/2567_1_Q3_A2-S.py
@@ -1,17 +1,24 @@
-# Input initial money and amount of months
-money, months = input().split()
-money, months = float(money), int(months)
+# --------------------------------------------------
+# File Name : 2567_1_Q3_A2-S.py
+# Problem   : Interest Rate
+# Author    : Worralop Srichainont
+# Date      : 2025-07-28
+# --------------------------------------------------
 
-# Calculate money
-# m = 1,5,9,13,...  (m % 4 = 1) --> Got 1% interest
-# m = 2,6,10,14,... (m % 4 = 2) --> Got 2% interest
-# m = 3,7,11,15,... (m % 4 = 3) --> Got 3% interest
-# m = 4,8,12,16,... (m % 4 = 0) --> Got 4% interest
-for m in range(1,months+1):
-    if(m % 4 == 0):
-        money += (money * (4/100) * (1/12))
-    else:
-        money += (money * ((m % 4)/100) * (1/12))
+# Interest rates for each month
+INTEREST_RATE = [0.04, 0.01, 0.02, 0.03]
+
+# Extract initial money and total months from the input
+data = input().split()
+initial_money = float(data[0])
+total_months = int(data[1])
+
+# Initialize current money with the initial money
+current_money = initial_money
+
+# Calculate the money after each month using the interest rates
+for month in range(1, total_months + 1):
+    current_money += current_money * INTEREST_RATE[month % 4]
 
 # Output
-print(round(money,2))
+print(round(current_money, 2))
