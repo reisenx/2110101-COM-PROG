cc15c286a53e2ba667fe8308aa4edb81eb5ecaea
Worralop Srichainont <worralop.sric@gmail.com>
2025-07-29 13:54:37 +0700

    feat: formatting 2567_1_q2_a2-s solution

--- GE-Grader-Examination/G6701-Exam-2567-S1/Grader-02-Survey/2567_1_Q2_A2-S/2567_1_Q2_A2-S.py
+++ GE-Grader-Examination/G6701-Exam-2567-S1/Grader-02-Survey/2567_1_Q2_A2-S/2567_1_Q2_A2-S.py
@@ -1,30 +1,41 @@
-# Input
-debt = float(input())
-rate = float(input())
-monthlyPay = float(input())
+# --------------------------------------------------
+# File Name : 2567_1_Q2_A2-S.py
+# Problem   : Loan Debt
+# Author    : Worralop Srichainont
+# Date      : 2025-07-28
+# --------------------------------------------------
 
-# ----- Formula -----
-# interestMoney = debt * (rate/12)
-# debt = debt - (monthlyPay - interestMoney)
-# --------------------
-months = 0
-interestPay = 0
-# Definite case (monthlyPay more than interestMoney)
-if(monthlyPay > (debt*((rate/12)/100))):
-    # Loop until debt = 0
-    while(debt > 0):
-        # Calculate interestMoney
-        interestMoney = debt * ((rate/12)/100)
-        # Last month
-        if((debt + interestMoney) < monthlyPay):
-            monthlyPay = debt + interestMoney
-        # Remainder debt
-        debt -= (monthlyPay - interestMoney)
-        # Calculate interestPay
-        interestPay += interestMoney
-        months += 1
-    # Output
-    print(months, round(interestPay, 2))
-# Indefinite case (monthlyPay not more than interestMoney)
+# Input initial debt, interest rate, and monthly payment
+initial_debt = float(input())
+interest_rate = float(input()) / 100
+monthly_pay = float(input())
+
+# Initialize total month and total interest
+total_month = 0
+total_interest = 0
+
+# Initialize current debt and calculate initial interest
+current_debt = initial_debt
+interest = current_debt * (interest_rate / 12)
+
+# Check if monthly payment is greater than interest
+if monthly_pay > interest:
+    # Loop until the debt is paid off
+    while current_debt > 0:
+        # Calculate interest of the current month
+        interest = current_debt * (interest_rate / 12)
+
+        # Update current debt and total interest
+        current_debt = max(0, current_debt - (monthly_pay - interest))
+        total_interest += interest
+
+        # Increment total month
+        total_month += 1
+
+    # Output total month and total interest
+    print(total_month, round(total_interest, 2))
+
+# If monthly payment is less than or equal to interest
+# it's impossible to pay off the debt
 else:
     print("indefinite")
