5616201d58212866b6ae7256f59f266b8ad24b61
Worralop Srichainont <worralop.sric@gmail.com>
2025-07-31 11:49:23 +0700

    feat: formatting 2567_2_q1_a2 solution

--- GE-Grader-Examination/G6702-Exam-2567-S2/Grader-01/2567_2_Q1_A2/2567_2_Q1_A2.py
+++ GE-Grader-Examination/G6702-Exam-2567-S2/Grader-01/2567_2_Q1_A2/2567_2_Q1_A2.py
@@ -1,44 +1,40 @@
-# Input
-T_old = float(input())
-T_room = float(input())
-delta_t = float(input())
+# --------------------------------------------------
+# File Name : 2567_2_Q1_A2.py
+# Problem   : Newton's Law of Cooling
+# Author    : Worralop Srichainont
+# Date      : 2025-07-29
+# --------------------------------------------------
+
+# Initialization of constants
+K1 = [0.05, 0.02, 0.01, 0.015]
+K2 = [1.5, 1.0, 0.8]
+
+# Input temperatures information
+temp_old = float(input())
+temp_room = float(input())
+
+# Input time interval
+time_interval = float(input())
+
+# Input material and wind type
 material_type = int(input())
 wind_type = int(input())
 
-# Calculate k
-k1 = [0.05, 0.02, 0.01, 0.015]
-k2 = [1.5, 1.0, 0.8]
-k = k1[material_type - 1] * k2[wind_type - 1]
+# Calculate k constant by material and wind type
+k = K1[material_type - 1] * K2[wind_type - 1]
 
-# Calculate total_time and T_final
-total_time = delta_t
-T_new = T_old - k * (T_old - T_room) * delta_t
-# Loop until the temperature difference is less than 10^-3
-while(abs(T_old - T_new) >= 10**-3):
-    total_time += delta_t
-    T_old = T_new
-    T_new = T_old - k * (T_old - T_room) * delta_t
+# Initialize total time and new temperature
+total_time = time_interval
+temp_new = temp_old - k * (temp_old - temp_room) * time_interval
 
-# Output
-print(round(total_time, 2), round(T_new, 3))
+# Calculate the time until the temperature difference is less than 0.001
+while abs(temp_old - temp_new) >= 10**-3:
+    # Update total time
+    total_time += time_interval
 
-# Material Type
-# | Type | Material |  k1   |
-# |------|----------|-------|
-# |  1   |  Metal   | 0.05  |
-# |  2   |  Water   | 0.02  |
-# |  3   |  Plastic | 0.01  |
-# |  4   |  Others  | 0.015 |
-# ---------------------------
+    # Update temperatures
+    temp_old = temp_new
+    temp_new = temp_old - k * (temp_old - temp_room) * time_interval
 
-# Wind Type
-# | Type | Velocity |  k2  |
-# |------|----------|------|
-# |  1   | > 5 m/s  | 1.5  |
-# |  2   | 1-5 m/s  | 1.0  |
-# |  3   | < 1 m/s  | 0.8  |
-# --------------------------
-
-# Newton's Law of Cooling Formula
-# k = k1 * k2
-# T_new = T_old - k * (T_old - T_room) * delta_t
+# Output the total time and the final temperature
+print(round(total_time, 2), round(temp_new, 3))
