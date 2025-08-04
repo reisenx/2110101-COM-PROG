7a1d5b785c8f83a61edc548a9119f74b0e012367
Worralop Srichainont <worralop.sric@gmail.com>
2025-08-04 10:24:23 +0700

    fix: variable type assignment

--- 02-Basic-String-and-List/02_StrList_07/02_StrList_07.py
+++ 02-Basic-String-and-List/02_StrList_07/02_StrList_07.py
@@ -10,18 +10,15 @@ num = input().strip()
 
 # Step 1: Concatenate the numbers at indices 3, 10, 17, 24, and 31
 #         and convert them to an integer.
-step1 = num[3] + num[10] + num[17] + num[24] + num[31]
-step1 = int(step1)
+step1 = int(num[3::7])
 
 # Step 2: Concatenate the numbers at indices 7, 12, 17, 22, and 27
 #         and convert them to an integer.
-step2 = num[7] + num[12] + num[17] + num[22] + num[27]
-step2 = int(step2)
+step2 = int(num[7::5])
 
 # Step 3: Add the results from Step 1 and Step 2, then add 10000
 #         and convert the result to a string.
-step3 = step1 + step2 + 10000
-step3 = str(step3)
+step3 = str(step1 + step2 + 10000)
 
 # Step 4: Take the middle three digits from the result of Step 3
 step4 = step3[-4:-1]
@@ -29,11 +26,11 @@ step4 = step3[-4:-1]
 # Step 5: Calculate the sum of the digits in Step 4,
 #         choose the last digit, and add 1.
 step5 = int(step4[0]) + int(step4[1]) + int(step4[2])
-step5 = step5 % 10 + 1
+step5 = (step5 % 10) + 1
 
 # Step 6: Use the result from Step 5 to select a letter from the alphabet
 ALPHABET = "ABCDEFGHIJ"
 step6 = ALPHABET[step5 - 1]
 
 # Step 7: Output the final result by concatenating Step 4 and Step 6
-print(step4 + step6)
+print(f"{step4}{step6}")
