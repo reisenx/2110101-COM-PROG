b71180b60e72c148bdefb131b061b8e0322a4e8f
Worralop Srichainont <worralop.sric@gmail.com>
2025-08-04 12:57:33 +0700

    fix: variable and code refactor

--- 03-Selection/03_If_02/03_If_02.py
+++ 03-Selection/03_If_02/03_If_02.py
@@ -6,32 +6,36 @@
 # --------------------------------------------------
 
 # Input two students' information
-ID01, GPAX01, COM01, CAL1_01, CAL2_01 = input().strip().split()
-ID02, GPAX02, COM02, CAL1_02, CAL2_02 = input().strip().split()
-GPAX01, GPAX02 = float(GPAX01), float(GPAX02)
+data = input().strip().split()
+id_01, com_01, calculus1_01, calculus2_01 = data[0], data[2], data[3], data[1]
+gpax_01 = float(data[1])
+
+data = input().strip().split()
+id_02, com_02, calculus1_02, calculus2_02 = data[0], data[2], data[3], data[1]
+gpax_02 = float(data[1])
 
 # Set initial values for success of change of major
-# A student must have COM equal to A and CAL at least C
-success01 = COM01 == "A" and CAL1_01 <= "C" and CAL2_01 <= "C"
-success02 = COM02 == "A" and CAL1_02 <= "C" and CAL2_02 <= "C"
+# A student must have COM equal to A and CALCULUS at least C
+is_qualified_01 = (com_01 == "A") and (calculus1_01 <= "C") and (calculus2_01 <= "C")
+is_qualified_02 = (com_02 == "A") and (calculus1_02 <= "C") and (calculus2_02 <= "C")
 
 # Output the result for each student
-if success01 and success02:
-    # Compare GPAX then CAL1 and CAL2
-    student01 = [-GPAX01, CAL1_01, CAL2_01]
-    student02 = [-GPAX02, CAL1_02, CAL2_02]
-    if student01 < student02:
-        print(ID01)
-    elif student01 > student02:
-        print(ID02)
+if is_qualified_01 and is_qualified_02:
+    # Compare GPAX then CALCULUS 1 and CALCULUS 2
+    student_01 = [-gpax_01, calculus1_01, calculus2_01]
+    student_02 = [-gpax_02, calculus1_02, calculus2_02]
+    if student_01 < student_02:
+        print(id_01)
+    elif student_01 > student_02:
+        print(id_02)
     else:
         print("Both")
 
-elif success01:
-    print(ID01)
+elif is_qualified_01:
+    print(id_01)
 
-elif success02:
-    print(ID02)
+elif is_qualified_02:
+    print(id_02)
 
 else:
     print("None")
--- 03-Selection/03_If_F02/03_If_F02.py
+++ 03-Selection/03_If_F02/03_If_F02.py
@@ -6,34 +6,35 @@
 # --------------------------------------------------
 
 
-def choose(stu1: list, stu2: list) -> list:
-    # Initialize the result list
-    result = []
+def choose(student_01, student_02):
+    # Assign variables
+    id_01, gpax_01, com_01, calculus1_01, calculus2_01 = student_01
+    id_02, gpax_02, com_02, calculus1_02, calculus2_02 = student_02
 
     # Set initial values for success of change of major
     # A student must have COM equal to A and CAL at least C
-    success01 = stu1[2] == "A" and stu1[3] <= "C" and stu1[4] <= "C"
-    success02 = stu2[2] == "A" and stu2[3] <= "C" and stu2[4] <= "C"
+    is_qualified_01 = com_01 == "A" and calculus1_01 <= "C" and calculus2_01 <= "C"
+    is_qualified_02 = com_02 == "A" and calculus1_02 <= "C" and calculus2_02 <= "C"
 
     # Find the result of change of major
-    if success01 and success02:
+    if is_qualified_01 and is_qualified_02:
         # Compare GPAX then CAL1 and CAL2
-        student01 = [-stu1[1], stu1[3], stu1[4]]
-        student02 = [-stu2[1], stu2[3], stu2[4]]
-        if student01 < student02:
-            result += [stu1[0]]
-        elif student01 > student02:
-            result += [stu2[0]]
-        else:
-            result += [stu1[0], stu2[0]]
+        grades_01 = [-gpax_01, calculus1_01, calculus2_01]
+        grades_02 = [-gpax_02, calculus1_02, calculus2_02]
 
-    elif success01:
-        result += [stu1[0]]
+        if grades_01 < grades_02:
+            return [id_01]
+        if grades_01 > grades_02:
+            return [id_02]
+        return [id_01, id_02]
 
-    elif success02:
-        result += [stu2[0]]
+    if is_qualified_01:
+        return [id_01]
 
-    return result
+    if is_qualified_02:
+        return [id_02]
+
+    return []
 
 
 # Execute the input string as code
